aba31f7add6626b8deabed9f33a3fc0145e3bb2f
Lucas Rodrigues da Silva <89489811+LucasRodriguesSilva@users.noreply.github.com>
2021-11-28 14:02:23 -0300

    Delete avancando/avancando directory

--- avancando/avancando/remover.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$numero = [1,2,3];
-
-unset ($numero[1]);
-
-foreach ($numero as $teste){
-    echo ($teste).PHP_EOL;
-};
--- avancando/avancando/tipos-chaves.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$array = [
-    1 => "a"
-];
-
-foreach ($array as $item){
-    echo ;
-}
